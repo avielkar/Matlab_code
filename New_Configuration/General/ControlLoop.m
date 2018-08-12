b09219adef4e698d1bcf9db863530cdc776e5bc4
avielkar <avraham.elkar@gmail.com>
2018-08-12 20:00:28 +0300

    Make the code impler.

--- New_Configuration/General/ControlLoop.m
+++ New_Configuration/General/ControlLoop.m
@@ -455,13 +455,11 @@ if ~paused && flagdata.isStopButton == 0
         %% Send the FLAH_SQUARE_DATA if it is prior, else send a vector of all ones.\
         %initialize the vector to be with 1's, so that all the frames
         %appear with the fixation point.
-        flash_square_data = ones(1,60)
-        
+        flash_square_data = ones(1,60)    
         if(cldata.prior_now == 1)
             %decide in which fram the square disappear.
             flash_time = data.configinfo(iFP_FLASH_TIME).parameters;
             flash_frame = randi([2 , 59 - flash_time] , 1);
-            
             %change that fram so that it would flash.
             flash_square_data(flash_frame : 1 : flash_frame + flash_time) = 0;
         else
