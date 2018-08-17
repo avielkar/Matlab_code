2ded6d3a5fab1b841b54e300665a2e4d47726078
avielkar <avraham.elkar@gmail.com>
2018-08-17 12:58:28 +0300

    Spelling parameter name fix.

--- New_Configuration/General/ControlLoop.m
+++ New_Configuration/General/ControlLoop.m
@@ -521,7 +521,7 @@ if ~paused && flagdata.isStopButton == 0
                 %save to the cldata the number of flashes.
                 cldata.num_of_flashes = num_of_flashes;
                 %save to the cldata the vector of flash square fixation point.
-                cldata.flash_vector_data = flash_square_data;
+                cldata.flash_square_data = flash_square_data;
             end
         else
             %save in the control loop data that the prior trial is
